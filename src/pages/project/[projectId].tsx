0397d73ccb2fe5fd35117aee60681713566401db
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-09-16 21:29:03 -0300

    feat: integrated pomodoro-timer project
    Integrated Pomodoro Timer project as submodule

---
+++ src/pages/project/[projectId].tsx
@@ -0,0 +1,83 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+import Page from "@/components/Page";
+import Surface from "@/components/Surface";
+import { projectsMetadata } from "@/shared/projects";
+import useThemeColor from "@/hooks/useThemeColor";
+import useGlobalSettings from "@/hooks/useGlobalSettings";
+
+interface ProjectPageProps {
+  projectId: string;
+}
+
+interface AppSetupProps {
+  containerId: string;
+  basename?: string;
+  isSubApp?: boolean;
+}
+
+type AppSetupFunction = (props: AppSetupProps) => void;
+
+export const getStaticPaths = (() => {
+  return {
+    paths: projectsMetadata.map((p) => ({ params: { projectId: p.id } })),
+    fallback: false,
+  };
+}) satisfies GetStaticPaths;
+
+export const getStaticProps = (async ({ params }) => {
+  return {
+    props: {
+      projectId: params!.projectId as string,
+    },
+  };
+}) satisfies GetStaticProps<ProjectPageProps>;
+
+function ProjectPage({ projectId }: ProjectPageProps) {
+  const { t } = useTranslation();
+
+  const borderColor = useThemeColor("border.500");
+  const { setViewingProjectId } = useGlobalSettings();
+
+  useEffect(() => {
+    setViewingProjectId(projectId);
+
+    (async () => {
+      const setupApp: AppSetupFunction = (
+        await import(`@/../projects/${projectId}/src/setupApp`)
+      ).default;
+
+      setupApp({
+        containerId: "subapp-root",
+        basename: `/project/${projectId}`,
+        isSubApp: true,
+      });
+    })();
+    return () => setViewingProjectId(null);
+  }, [projectId]);
+
+  const project = projectsMetadata.find(
+    (metadata) => metadata.id === projectId
+  );
+
+  if (!project) return null;
+
+  const name = t(`projects.${projectId}.name`);
+
+  return (
+    <Page title={name}>
+      <Surface
+        flexGrow={1}
+        p={2}
+        borderStyle="solid"
+        borderWidth="1px"
+        borderColor={borderColor}
+        id="subapp-root"
+      />
+    </Page>
+  );
+}
+
+export default ProjectPage;
