c8ba890b787bead5db578f83aea8acecb2a2b27d
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-09-22 19:29:25 -0300

    fix: added i18n to project page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { ReactNode, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
+import Link from "next/link";
 
 import { BOTTOM_MENU_HEIGHT } from "@/shared/constants";
 import useGlobalSettings from "@/hooks/useGlobalSettings";
@@ -48,12 +49,18 @@ function Layout({ children }: LayoutProps) {
             gap={2}
             h="48px"
             flexShrink={0}
+            pb={4}
           >
             {viewingProjectId != null && (
-              <Text>
-                Viewing{" "}
-                <strong>{t(`projects.${viewingProjectId}.name`)}</strong>
-              </Text>
+              <Flex flexDir="column" alignItems="end">
+                <Text>
+                  {t("pages.singleProject.viewing")}{" "}
+                  <strong>{t(`projects.${viewingProjectId}.name`)}</strong>
+                </Text>
+                <Link href="/projects">
+                  <Text textDecoration="underline" color="pink.200">{t("pages.singleProject.goBackToProjects")}</Text>
+                </Link>
+              </Flex>
             )}
             {!sidebarOpen && <OpenSidebarButton />}
           </Flex>
